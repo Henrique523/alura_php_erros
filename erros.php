ff31231e75adced2194d89cf2ffa2bb54fe5170c
Henrique <suporte@pcmsolucoes.com.br>
2020-05-27 16:38:25 -0300

    Fim do curso.

---
+++ erros.php
@@ -0,0 +1,18 @@
+<?php
+
+error_reporting(E_ALL);
+
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    switch ($errno) {
+        case E_WARNING:
+            echo "Aviso: Isso é perigoso";
+            break;
+        case E_NOTICE:
+            echo "Melhor não fazer isso";
+            break;
+    }
+});
+
+echo $variavel;
+echo $array[12];
+echo CONSTANTE;
